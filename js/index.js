3139d4c51767b328148cd27300e3d129e0417586
mirror1125 <mirrorseek@gmail.com>
2019-11-02 23:32:04 +0900

    set status from json file

--- js/index.js
+++ js/index.js
@@ -1,16 +1,34 @@
-window.onload = function(){
-  getJSON();
+window.onload = function () {
+	viewOnLoad();
 }
 
-function getJSON() {
+function viewOnLoad() {
 	var req = new XMLHttpRequest();						// XMLHttpRequest オブジェクトを生成する
-	req.onreadystatechange = function() {				// XMLHttpRequest オブジェクトの状態が変化した際に呼び出されるイベントハンドラ
-		if(req.readyState == 4 && req.status == 200){	// サーバーからのレスポンスが完了し、かつ、通信が正常に終了した場合
+	req.onreadystatechange = function () {				// XMLHttpRequest オブジェクトの状態が変化した際に呼び出されるイベントハンドラ
+		if (req.readyState == 4 && req.status == 200) {	// サーバーからのレスポンスが完了し、かつ、通信が正常に終了した場合
 
-			var data = JSON.parse(req.responseText);	// 取得した JSON ファイルの中身を変数へ格納
-      console.log(data);
+			var profile = JSON.parse(req.responseText);	// 取得した JSON ファイルの中身を変数へ格納
+
+			setStatus(profile);
+			setSkill(profile);
+
 		}
 	};
-	req.open("GET", "./json/status.json", false);				// HTTPメソッドとアクセスするサーバーのURLを指定
+	req.open("GET", "./json/status.json", false);		// HTTPメソッドとアクセスするサーバーのURLを指定
 	req.send(null);										// 実際にサーバーへリクエストを送信
+}
+
+function setStatus(profile) {
+	document.getElementById("name").innerHTML = profile["name"];
+	document.getElementById("level").innerHTML = profile["level"];
+	document.getElementById("hp").innerHTML = profile["hp"];
+	document.getElementById("mp").innerHTML = profile["mp"];
+	document.getElementById("power").innerHTML = profile["power"];
+	document.getElementById("strength").innerHTML = profile["strength"];
+	document.getElementById("quickness").innerHTML = profile["quickness"];
+	document.getElementById("wisdom").innerHTML = profile["wisdom"];
+}
+
+function setSkill(profile) {
+
 }
